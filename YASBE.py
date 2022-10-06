a6df582b4834c6207df9cb0f4ad853021c12ed9c
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2022-10-06 08:04:34 -0600

    Update YASBE.py

--- YASBE.py
+++ YASBE.py
@@ -1,6 +1,9 @@
-# YASBE Interpreter 1.0
+# YASBE Interpreter 1.1
 
 import sys
+
+def end_program(reason):
+    return f"\n\n== PROGRAM ENDED == \nProgram execution finished: {reason}"
 
 try:
     with open(" ".join(sys.argv[2:])) as codefile:
@@ -8,7 +11,7 @@ try:
 except Exception:
     code = input("Code: ")
 
-tokens = {"push":0, "pop":1, "copy":2, "flip":3, "add":4, "subt":5, "mult":6, "div":7, "char":8, "value":9, "cget":10, "iget":11, "jump":12, "condjump":13}
+tokens = {"push":0, "pop":1, "copy":2, "flip":3, "add":4, "subt":5, "mult":6, "div":7, "char":8, "value":9, "cget":10, "iget":11, "jump":12, "condjump":13, "break":14, "condbreak":15, "swap":16, "pushback":17, "debug":999}
 tape = [0] * 1000
 
 def tokenize(code):
@@ -39,7 +42,11 @@ def interpret(code):
             stack.append(code[pointer])
         
         if opcode == 1: # pop
-            stack.pop()
+            try:
+                stack.pop()
+            except IndexError:
+                print(end_program("Pop from empty stack"))
+                return 0
         
         if opcode == 2: # copy
             stack.append(stack[-1])
@@ -92,8 +99,31 @@ def interpret(code):
             pointer += 1
             addr = code[pointer] - 1
             pointer += 1
-            if stack[-1] != code[pointer]:
+            if stack[-1] != stack[code[pointer]]:
                 pointer = addr
+        
+        if opcode == 14: # break
+            print(end_program("Break instruction reached"))
+            return 0
+        
+        if opcode == 15: # condbreak
+            pointer += 1
+            
+            if stack[-1] != stack[code[pointer]]:
+                print(end_program("Break instruction reached"))
+                return 0
+        
+        if opcode == 16: # swap
+            buf = stack[0]
+            stack[0] = stack[-1]
+            stack[-1] = buf
+        
+        if opcode == 17: # pushback
+            pointer += 1
+            stack.insert(0, code[pointer])
+        
+        if opcode == 999: # debug
+            print(stack)
 
         pointer += 1
 
